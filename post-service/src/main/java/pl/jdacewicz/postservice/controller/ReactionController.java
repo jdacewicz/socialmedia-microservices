cba9f6cc72fdc8d8506bcebb9e7864f69326a219
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-06-21 15:43:12 +0200

    Implemented getting and updating Reactions

--- post-service/src/main/java/pl/jdacewicz/postservice/controller/ReactionController.java
+++ post-service/src/main/java/pl/jdacewicz/postservice/controller/ReactionController.java
@@ -1,34 +1,61 @@
 package pl.jdacewicz.postservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import pl.jdacewicz.postservice.dto.ReactionDto;
 import pl.jdacewicz.postservice.dto.ReactionRequest;
-import pl.jdacewicz.postservice.entity.Reaction;
+import pl.jdacewicz.postservice.dto.mapper.ReactionMapper;
+import pl.jdacewicz.postservice.model.Reaction;
 import pl.jdacewicz.postservice.service.ReactionService;
 
-import java.util.List;
-
 @RestController
+@Transactional
 @RequestMapping(value = "/api/reactions", headers = "Accept=application/json")
 public class ReactionController {
 
     private final ReactionService reactionService;
+    private final ReactionMapper reactionMapper;
 
     @Autowired
-    public ReactionController(ReactionService reactionService) {
+    public ReactionController(ReactionService reactionService, ReactionMapper reactionMapper) {
         this.reactionService = reactionService;
+        this.reactionMapper = reactionMapper;
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ReactionDto getReaction(@PathVariable int id) {
+        Reaction reaction = reactionService.getReactionById(id);
+        return reactionMapper.convertToDto(reaction);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<Reaction> getAllReactions() {
-        return reactionService.getAllReactions();
+    public Page<ReactionDto> getAllReactions(@RequestParam(required = false) String name,
+                                             @RequestParam(defaultValue = "0") int page,
+                                             @RequestParam(defaultValue = "8") int size,
+                                             @RequestParam(defaultValue = "id") String sort,
+                                             @RequestParam(defaultValue = "false") boolean desc) {
+        return reactionService.getReactions(name, page, size, sort, desc)
+                .map(reactionMapper::convertToDto);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Reaction createReaction(@RequestBody ReactionRequest reactionRequest) {
-        return reactionService.createReaction(reactionRequest);
+    public ReactionDto createReaction(@RequestBody ReactionRequest reactionRequest) {
+        Reaction reaction = reactionMapper.convertFromRequest(reactionRequest);
+        Reaction createdReaction =  reactionService.createReaction(reaction);
+        return reactionMapper.convertToDto(createdReaction);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateReaction(@PathVariable int id,
+                                      @RequestBody ReactionRequest reactionRequest) {
+        Reaction reaction = reactionMapper.convertFromRequest(reactionRequest);
+        reactionService.updateReaction(id, reaction);
     }
 }
--- post-service/src/main/java/pl/jdacewicz/postservice/service/ReactionService.java
+++ post-service/src/main/java/pl/jdacewicz/postservice/service/ReactionService.java
@@ -1,12 +1,15 @@
 package pl.jdacewicz.postservice.service;
 
+import jakarta.persistence.Transient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import pl.jdacewicz.postservice.dto.ReactionRequest;
-import pl.jdacewicz.postservice.entity.Reaction;
+import pl.jdacewicz.postservice.exception.RecordNotFoundException;
+import pl.jdacewicz.postservice.model.Reaction;
 import pl.jdacewicz.postservice.repository.ReactionRepository;
-
-import java.util.List;
 
 @Service
 public class ReactionService {
@@ -18,14 +21,28 @@ public class ReactionService {
         this.reactionRepository = reactionRepository;
     }
 
-    public List<Reaction> getAllReactions() {
-        return reactionRepository.findAll();
+    public Reaction getReactionById(int id) {
+        return reactionRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException("Could not find reaction with id: " + id));
     }
 
-    public Reaction createReaction(ReactionRequest reactionRequest) {
-        Reaction reaction = Reaction.builder()
-                .name(reactionRequest.name())
-                .build();
+    public Page<Reaction> getReactions(String name, int page, int size, String sort, boolean desc) {
+        Sort pageSort = (desc) ? Sort.by(sort).descending() : Sort.by(sort).ascending();
+        Pageable paging = PageRequest.of(page, size, pageSort);
+
+        if (name == null || name.isEmpty()) {
+            return reactionRepository.findAll(paging);
+        } else {
+            return reactionRepository.findAllByName(name, paging);
+        }
+    }
+
+    public Reaction createReaction(Reaction reaction) {
         return reactionRepository.save(reaction);
+    }
+
+    @Transient
+    public void updateReaction(int id, Reaction reaction) {
+        reactionRepository.setReactionById(id, reaction);
     }
 }
