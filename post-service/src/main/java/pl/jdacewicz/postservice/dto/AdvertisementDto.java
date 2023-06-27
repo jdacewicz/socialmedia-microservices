7643663f76417c5fc78717bfc16222c904d3b586
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-06-27 18:39:36 +0200

    Created Advertisement dto, request and mapper

---
+++ post-service/src/main/java/pl/jdacewicz/postservice/dto/AdvertisementDto.java
@@ -0,0 +1,21 @@
+package pl.jdacewicz.postservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdvertisementDto {
+
+   private String content;
+
+   private List<ReactionCountDto> reactions;
+
+   private List<CommentDto> comments;
+}
