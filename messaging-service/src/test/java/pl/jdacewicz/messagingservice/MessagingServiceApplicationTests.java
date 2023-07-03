b90ae35e94a10dd42f4a6cb72293c71b2ed36501
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-07-03 17:21:16 +0200

    Messaging service init

---
+++ messaging-service/src/test/java/pl/jdacewicz/messagingservice/MessagingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jdacewicz.messagingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
