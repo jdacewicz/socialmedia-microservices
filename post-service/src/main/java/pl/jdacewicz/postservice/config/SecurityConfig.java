435b1835fb24ae7808afa7af53ef0af1c2e904d6
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-07-06 20:59:19 +0200

    Added security to Post Service

---
+++ post-service/src/main/java/pl/jdacewicz/postservice/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package pl.jdacewicz.postservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                        .authorizeHttpRequests(requests -> requests
+                                .anyRequest().authenticated())
+                                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return httpSecurity.build();
+    }
+}
