b8043f67b52ceac8ff360e347eb96bac043d1294
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-07-10 19:18:53 +0200

    Clearing sharing service

--- sharing-service/src/main/java/pl/jdacewicz/sharingservice/SharingServiceApplication.java
+++ sharing-service/src/main/java/pl/jdacewicz/sharingservice/SharingServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PostServiceApplication {
+public class SharingServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PostServiceApplication.class, args);
+        SpringApplication.run(SharingServiceApplication.class, args);
     }
 
 }
