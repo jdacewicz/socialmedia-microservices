68b332cde7ab8e40cbd29b2228e6d01cf36d1c17
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-07-10 18:00:46 +0200

    Methods authorisation

--- api-gateway/src/main/java/pl/jdacewicz/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/pl/jdacewicz/apigateway/config/SecurityConfig.java
@@ -2,21 +2,34 @@ package pl.jdacewicz.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.util.Collection;
+import java.util.Map;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers("/eureka/**").permitAll()
                         .anyExchange().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer(oauth2Config -> oauth2Config.jwt(jwtConfig -> jwtConfig.jwtAuthenticationConverter(jwt -> {
+                    Map<String, Collection<String>> realmAccess = jwt.getClaim("realm_access");
+                    Collection<String> roles = realmAccess.get("roles");
+                    var grantedAuthorities = roles.stream()
+                            .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                            .toList();
+                    return Mono.just(new JwtAuthenticationToken(jwt, grantedAuthorities));
+                })));
         return serverHttpSecurity.build();
     }
 }
