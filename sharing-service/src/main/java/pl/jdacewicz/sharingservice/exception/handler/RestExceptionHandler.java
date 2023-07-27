3177d41955fc12fa3428ba9ed958877e5c942f1d
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-07-27 21:32:08 +0200

    Customized validation message

--- sharing-service/src/main/java/pl/jdacewicz/sharingservice/exception/handler/RestExceptionHandler.java
+++ sharing-service/src/main/java/pl/jdacewicz/sharingservice/exception/handler/RestExceptionHandler.java
@@ -1,16 +1,25 @@
 package pl.jdacewicz.sharingservice.exception.handler;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import pl.jdacewicz.sharingservice.exception.NotUniqueDataException;
 import pl.jdacewicz.sharingservice.exception.RecordNotFoundException;
 import pl.jdacewicz.sharingservice.model.ApiError;
+
+import java.util.List;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
@@ -31,4 +40,19 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         ApiError apiError = new ApiError(status.value(), ex.getMessage());
         return new ResponseEntity<>(apiError, status);
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, @NonNull HttpHeaders headers,
+                                                                  HttpStatusCode status, @NonNull WebRequest request) {
+        BindingResult result = ex.getBindingResult();
+        List<String> errorList = result.getFieldErrors()
+                .stream()
+                .map(e -> e.getField() + ": " + e.getDefaultMessage())
+                .toList();
+
+        String errorString = StringUtils.join(errorList, ", ");
+        ApiError apiError = new ApiError(status.value(), errorString);
+        return new ResponseEntity<>(apiError, status);
+    }
+
 }
