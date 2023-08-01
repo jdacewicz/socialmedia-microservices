6607a6e089f66f03f82e9585a25642daa8a88e60
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-08-01 21:14:27 +0200

    Tests of AdvertisementService c.d.

--- sharing-service/src/test/java/pl/jdacewicz/sharingservice/service/AdvertisementServiceTest.java
+++ sharing-service/src/test/java/pl/jdacewicz/sharingservice/service/AdvertisementServiceTest.java
@@ -9,12 +9,15 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import pl.jdacewicz.sharingservice.exception.RecordNotFoundException;
 import pl.jdacewicz.sharingservice.model.Advertisement;
 import pl.jdacewicz.sharingservice.repository.AdvertisementRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -35,7 +38,7 @@ class AdvertisementServiceTest {
     @DisplayName("Given null name " +
             "When getting advertisements " +
             "Then should return Page of all advertisements")
-    void x() {
+    void gettingAdvertisementsByNullNameShouldReturnProperPage() {
         String name = null;
 
         Page<Advertisement> page = new PageImpl<>(List.of());
@@ -50,7 +53,7 @@ class AdvertisementServiceTest {
     @DisplayName("Given empty name " +
             "When getting advertisements " +
             "Then should return Page of all advertisements")
-    void x32() {
+    void gettingAdvertisementsByEmptyNameShouldReturnProperPage() {
         String name = "";
 
         Page<Advertisement> page = new PageImpl<>(List.of());
@@ -65,7 +68,7 @@ class AdvertisementServiceTest {
     @DisplayName("Given name " +
             "When getting advertisements " +
             "Then should return Page of all advertisements by name")
-    void x2() {
+    void gettingAdvertisementsByNameShouldReturnProperPage() {
         String name = "test";
 
         Page<Advertisement> page = new PageImpl<>(List.of());
@@ -74,5 +77,61 @@ class AdvertisementServiceTest {
         Page<Advertisement> result = advertisementService.getAdvertisements(name, 1,
                 1, "sort", "directory");
         assertEquals(page, result);
+    }
+
+    @Test
+    @DisplayName("Given existing id " +
+            "When getting advertisement " +
+            "Then should return advertisement")
+    void gettingAdvertisementByExistingIdShouldReturnAdvertisement() {
+        int id = 1;
+
+        Advertisement ad = new Advertisement();
+        when(advertisementRepository.findById(id)).thenReturn(Optional.of(ad));
+
+        Advertisement result = advertisementService.getAdvertisementById(id);
+
+        assertEquals(ad, result);
+    }
+
+    @Test
+    @DisplayName("Given not existing id " +
+            "When getting advertisement " +
+            "Then should throw RecordNotFoundException")
+    void gettingAdvertisementByNotExistingIdShouldThrowException() {
+        int id = 1;
+
+        when(advertisementRepository.findById(id)).thenThrow(RecordNotFoundException.class);
+
+        assertThrows(RecordNotFoundException.class,
+                () -> advertisementService.getAdvertisementById(id));
+    }
+
+    @Test
+    @DisplayName("Given existing id " +
+            "When getting active advertisement " +
+            "Then should throw RecordNotFoundException")
+    void gettingActiveAdvertisementByExistingIdShouldReturnAdvertisement() {
+        int id = 1;
+
+        Advertisement ad = new Advertisement();
+        when(advertisementRepository.findByIdAndActive(id, true)).thenReturn(Optional.of(ad));
+
+        Advertisement result = advertisementService.getActiveAdvertisementById(id);
+
+        assertEquals(ad, result);
+    }
+
+    @Test
+    @DisplayName("Given not existing id " +
+            "When getting active advertisement " +
+            "Then should throw RecordNotFoundException")
+    void gettingActiveAdvertisementByNotExistingIdShouldThrowException() {
+        int id = 1;
+
+        when(advertisementRepository.findByIdAndActive(id, true)).thenThrow(RecordNotFoundException.class);
+
+        assertThrows(RecordNotFoundException.class,
+                () -> advertisementService.getActiveAdvertisementById(id));
     }
 }
