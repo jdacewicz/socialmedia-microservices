d026af67bd6ae9b4fd5c600fc2bdaca0df78bd16
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-08-03 13:40:06 +0200

    Tests of UserService

--- sharing-service/src/test/java/pl/jdacewicz/sharingservice/service/UserServiceTest.java
+++ sharing-service/src/test/java/pl/jdacewicz/sharingservice/service/UserServiceTest.java
@@ -6,53 +6,83 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import pl.jdacewicz.sharingservice.exception.NotUniqueDataException;
+import pl.jdacewicz.sharingservice.exception.RecordNotFoundException;
 import pl.jdacewicz.sharingservice.model.User;
 import pl.jdacewicz.sharingservice.repository.UserRepository;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 class UserServiceTest {
 
     @InjectMocks
-    UserService userService;
+    UserService service;
 
     @Mock
-    UserRepository userRepository;
+    UserRepository repository;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
     }
 
+
     @Test
-    @DisplayName("Given user with unique email " +
-            "When creating user" +
-            "Then should return created user")
-    void creatingUserByNotUniqueEmailShouldReturnUser() {
+    @DisplayName("Given existing id " +
+            "When getting user " +
+            "Then should return user")
+    void gettingUserByExistingIdShouldReturnUser() {
+        long id = 1;
+
         User user = new User();
-        user.setEmail("test@test.com");
+        when(repository.findById(id)).thenReturn(Optional.of(user));
 
-        when(userRepository.save(user)).thenReturn(user);
+        User result = service.getUserById(id);
 
-        assertDoesNotThrow(() -> userService.createUser(user));
+        assertEquals(user, result);
     }
 
     @Test
-    @DisplayName("Given user with unique not unique email " +
-            "When creating user " +
-            "Then should throw NotUniqueDataException")
-    void creatingUserByUniqueEmailShouldThrowNotUniqueDataException() {
+    @DisplayName("Given not existing id " +
+            "When getting user " +
+            "Then should throw RecordNotFoundException")
+    void gettingUserByNotExistingIdShouldThrowException() {
+        long id = 1;
+
+        when(repository.findById(id)).thenThrow(RecordNotFoundException.class);
+
+        assertThrows(RecordNotFoundException.class,
+                () -> service.getUserById(id));
+    }
+
+    @Test
+    @DisplayName("Given existing email " +
+            "When getting user " +
+            "Then should return user")
+    void gettingUserByExistingEmailShouldReturnUser() {
+        String email = "test@test.com";
+
         User user = new User();
-        user.setEmail("test@test.com");
+        when(repository.findByEmail(email)).thenReturn(Optional.of(user));
 
-        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        User result = service.getUserByEmail(email);
 
-        assertThrows(NotUniqueDataException.class,
-                () -> userService.createUser(user));
+        assertEquals(user, result);
+    }
+
+    @Test
+    @DisplayName("Given not existing email " +
+            "When getting user " +
+            "Then should throw RecordNotFoundException")
+    void gettingUserByNotExistingEmailShouldThrowException() {
+        String email = "test@test.com";
+
+        when(repository.findByEmail(email)).thenThrow(RecordNotFoundException.class);
+
+        assertThrows(RecordNotFoundException.class,
+                () -> service.getUserByEmail(email));
     }
 }
